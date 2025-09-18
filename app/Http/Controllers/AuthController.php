af2fa65508072c8b8b6aa97e31981b1b178a3e76
ThuyNhanVp <lactroisoi@gmail.com>
2025-09-18 10:47:54 +0700

    fix msg Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,7 +69,7 @@ class AuthController extends Controller
 
     return response()->json([
         'success' => true,
-        'message' => 'Đăng ký thành công và đã đăng nhập!'
+        'message' => 'Đăng ký thành công đang chuyển sang đăng nhập!'
     ]);
 }
 
